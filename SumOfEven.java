b8299d920d790b32efdba844a3ebdc26e39b4c4c
Md. Meherub Hossain Yemon <100033382+mhyemon95@users.noreply.github.com>
2022-03-07 22:47:49 +0600

    Update SumOfEven.java

--- SumOfEven.java
+++ SumOfEven.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class SumOfEven {
 	public static void main(String[]args) {Scanner scanner = new Scanner(System.in);
 	System.out.print("Enter the number:");
-	int n = scanner.nextInt();
+	int N = scanner.nextInt();
 	int sum=0;
-	for(;n!=0;n=n/10) {
-		if((n%10)%2==0) {
-			sum+=n%10;
+	for(;N!=0;N=N/10) {
+		if((N%10)%2==0) {
+			sum+=N%10;
 		}
 	}
 	System.out.println(sum);
